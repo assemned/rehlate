7b1328454daa6940a3ddd2426a50305f72440026
assemned <assemned1@gmail.com>
2023-11-26 08:46:47 +0100

    update

--- app/sections/Navbar.js
+++ app/sections/Navbar.js
@@ -30,7 +30,7 @@ const Navbar = () => {
           href="/"
           className=" font-lamaMedium p1 flexEnd gap-1 hover:scale-110 duration-200">
           <p>يوتيوب </p>
-          <AiFillYoutube className="h4" />
+          <AiFillYoutube className="h4 max-sm:h3" />
         </a>
       </div>
     </nav>
